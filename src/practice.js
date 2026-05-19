ea069e3d1a26bb6abb9eea45fa2c4e9094d56f71
Student <hrachya.h13@tumo.org>
2026-05-19 20:29:35 +0400

    commit2

--- src/practice.js
+++ src/practice.js
@@ -30,4 +30,24 @@ console.log("Dot notation:")
 console.log(IssPosition.latitude);
 console.log(IssPosition.longitude);
 
+const asteroids = [
 
+    {name: "2024 AB1", diameter : 120, hazadours: false }
+    {name: "2024 CD2", diameter : 45, hazadours: true }
+     {name: "2024 EF3", diameter : 890, hazadours: false }
+    {name: "2024 GH4", diameter :23, hazadours: true };
+]
+
+const names = asteroids.map(asteroid => asteroid.name)
+console.log(names)
+
+const hazardousOnly = asteroids.filter(asteroid => asteroid.hazardous === true)
+console.log(hazardousOnly)
+
+const firstTwo =asteroids.slice (0,2)
+console.log(firstTwo)
+
+const hazardousNames = asteroids
+ .filter(asteroid => asteroid.hazardous === true )
+  .map(asteroid.nameames)
+  console.log(hazardousNames)
