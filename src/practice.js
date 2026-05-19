d0b758c251d098239ea2f0bc9c4c996aa5519560
Student <hrachya.h13@tumo.org>
2026-05-19 19:53:36 +0400

    commit

---
+++ src/practice.js
@@ -0,0 +1,33 @@
+const name = "HeloTUMO"
+let count =  7
+
+console.log (name)
+console.log(count)
+
+count = count + 77 
+console.log(count)
+
+let Miles= 77
+
+function kmTOMiles(km){
+    return km * 0.621;
+
+}
+console.log(kmTOMiles(Miles))
+
+
+function parametr (name,craft){
+'${name},is cuttently abard the,${craft'
+}
+console.log(parametr)
+
+
+const IssPosition = {
+    latitude: 45.5017,
+    longitude:-73.5673
+};
+console.log("Dot notation:")
+console.log(IssPosition.latitude);
+console.log(IssPosition.longitude);
+
+
